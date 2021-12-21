25286e9407d10edcb6b814dbd3951322ca1c5971
YOSHIDA Masanori <masanori.yoshida@gmail.com>
2021-12-21 17:58:45 +0900

    update the experiment in Python
    Signed-off-by: YOSHIDA Masanori <masanori.yoshida@gmail.com>

--- python/test.py
+++ python/test.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from scalar_pb2 import Scalar
+from nest_pb2 import GlobalA, GlobalB
 
 def make_scalar() -> Scalar:
   s = Scalar()
@@ -15,8 +16,21 @@ def make_scalar() -> Scalar:
   s.b = True      # 13
   return s
 
+def make_global_a() -> GlobalA:
+  ga = GlobalA()
+  ga.na.s = "HELLO"
+  ga.nb.s = "WORLD"
+  return ga
+
+def make_global_b() -> GlobalB:
+  gb = GlobalB()
+  gb.nb.s = "hello"
+  gb.na.s = "world"
+  return gb
+
 if __name__ == '__main__':
-  s = make_scalar()
-  b = s.SerializeToString()
-  # b = 15a4709d3f 1898f8ffffffffffffff01 30e807 38cf0f 4de8030000 6118fcffffffffffff 6801 720568656c6c6f 7a05776f726c64
-  print(b.hex())
+  # Scalar = 15a4709d3f 1898f8ffffffffffffff01 30e807 38cf0f 4de8030000 6118fcffffffffffff 6801 720568656c6c6f 7a05776f726c64
+  print('Scalar:', make_scalar().SerializeToString().hex())
+
+  print('GlobalA:', make_global_a().SerializeToString().hex())
+  print('GlobalB:', make_global_b().SerializeToString().hex())
